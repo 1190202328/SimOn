117981c2672c88aa88f01c9b341d71c152bd038a
1190202328 <1259029328@qq.com>
2023-02-26 11:16:27 +0800

    更新了脚本

--- thumos14_kinetics_run_scripts/eval_tal_with_pred_oad.sh
+++ thumos14_kinetics_run_scripts/eval_tal_with_pred_oad.sh
@@ -1,6 +1,15 @@
+## 新的结果
+#python tools/ontal_evaluator.py \
+#  --all_probs_file "models/onal_sequence_thumos14_decoder_layer4_kv7/validation_results/test_0_all_probs.pkl" \
+#  --ground_truth_cls "models/onal_sequence_thumos14_decoder_layer4_kv7/validation_results/test_0_ground_truth_cls.pkl" \
+#  --video_names_file "models/onal_sequence_thumos14_decoder_layer4_kv7/validation_results/test_0_video_names.pkl" \
+#  --ambiguos_info "data/thumos14_feat/ambiguous_dict.pkl" \
+#  --oad_gt_file "data/thumos14_feat/thumos_from_oad_multi_classes.pkl"
+
+# 原来最开始的结果
 python tools/ontal_evaluator.py \
-  --all_probs_file "models/onal_sequence_thumos14_decoder_layer4_kv7/validation_results/test_0_all_probs.pkl" \
-  --ground_truth_cls "models/onal_sequence_thumos14_decoder_layer4_kv7/validation_results/test_0_ground_truth_cls.pkl" \
-  --video_names_file "models/onal_sequence_thumos14_decoder_layer4_kv7/validation_results/test_0_video_names.pkl" \
+  --all_probs_file "models/origin_onal_sequence_thumos14_decoder_layer4_kv7/validation_results/test_0_all_probs.pkl" \
+  --ground_truth_cls "models/origin_onal_sequence_thumos14_decoder_layer4_kv7/validation_results/test_0_ground_truth_cls.pkl" \
+  --video_names_file "models/origin_onal_sequence_thumos14_decoder_layer4_kv7/validation_results/test_0_video_names.pkl" \
   --ambiguos_info "data/thumos14_feat/ambiguous_dict.pkl" \
   --oad_gt_file "data/thumos14_feat/thumos_from_oad_multi_classes.pkl"
--- thumos14_kinetics_run_scripts/test.sh
+++ thumos14_kinetics_run_scripts/test.sh
@@ -3,11 +3,22 @@ set -e
 export CUDA_VISIBLE_DEVICES=0
 export CUDA_LAUNCH_BLOCKING=1
 
+## 新的
+#python main.py \
+#  --output_dir "models/onal_sequence_thumos14_decoder_layer4_kv7" \
+#  --pickle_root "data/thumos14_feat" \
+#  --dim_feature 4096 \
+#  --history_desision 6 \
+#  --history_feature 6 \
+#  --epochs 17 --lr_drop 3 --gamma 0.5 \
+#  --eval --resume "models/onal_sequence_thumos14_decoder_layer4_kv7/checkpoint0016.pth"
+
+# 原来最开始的结果
 python main.py \
-  --output_dir "models/onal_sequence_thumos14_decoder_layer4_kv7" \
+  --output_dir "models/origin_onal_sequence_thumos14_decoder_layer4_kv7" \
   --pickle_root "data/thumos14_feat" \
   --dim_feature 4096 \
   --history_desision 6 \
   --history_feature 6 \
   --epochs 17 --lr_drop 3 --gamma 0.5 \
-  --eval --resume "models/onal_sequence_thumos14_decoder_layer4_kv7/checkpoint0016.pth"
+  --eval --resume "models/origin_onal_sequence_thumos14_decoder_layer4_kv7/checkpoint0016.pth"
